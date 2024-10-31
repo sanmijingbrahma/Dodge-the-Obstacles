da96422302549567ea33842799e538b9ab58417f
sanmijingbrahma <bsanmijing@gmail.com>
2024-10-31 09:29:55 +0530

    Define basic game structue and loop

--- main.cpp
+++ main.cpp
@@ -7,6 +7,27 @@ using namespace std;
 
 
 int main(){
+    const int screen_width = 800;
+    const int screen_height = 600;
+    const string name = "Dodge the obstacles!";
+
+    // initialize the window
+    InitWindow(screen_width,screen_height,name.c_str());
+
+
+
+    // game loop
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+
+        EndDrawing();
+    }
+
+    // close window
+    CloseWindow();
+
 
 
 
